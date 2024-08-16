6d85fd023dd09a6089720fa7f6dd8c568d4ebdbf
bing.you <bing.you@asiainnovations.net>
2024-08-16 12:59:12 +0800

    Fix: xcode16 类型不匹配报错修复

--- Source/CBridge.swift
+++ Source/CBridge.swift
@@ -27,7 +27,7 @@ import Foundation
 @_silgen_name("swift_getTypeByMangledNameInContext")
 public func _getTypeByMangledNameInContext(
     _ name: UnsafePointer<UInt8>,
-    _ nameLength: Int,
+    _ nameLength: UInt,
     genericContext: UnsafeRawPointer?,
     genericArguments: UnsafeRawPointer?)
     -> Any.Type?
